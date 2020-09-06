67a8cd22ce81fd3992569866cab23e3877ff7ea1
hamletswagstore <70469076+hamletswagstore@users.noreply.github.com>
2020-09-06 04:19:59 +0100

    JS

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -13,17 +13,17 @@ function getCountryCode() {
     Http.onreadystatechange = (e) => {
         if (Http.responseText === null || Http.responseText === '') {
             // default to check cookie present 
-            console.log("No response. Check cookie.")
+            // console.log("No response. Check cookie.")
             checkCookie();
         }
         else {
             var sGeoData = Http.responseText;
             var nCoCodePos = sGeoData.indexOf("loc=");
             var sCoCode = sGeoData.substring(nCoCodePos+4, nCoCodePos+6);
-            console.log(sCoCode);
+            // console.log(sCoCode);
             // not US, check for cookie present
             if (sCoCode != 'US')  {
-                console.log("Not US. Check cookie.")
+                // console.log("Not US. Check cookie.")
                 checkCookie(); 
             }
             if (document.getElementById('loc-currency').innerHTML) {
@@ -38,11 +38,11 @@ function checkCookie() {
 
     const x = getCookie('Elsinore');
     if (x) {
-        console.log("Found 'Elsinore' cookie.")
+        // console.log("Found 'Elsinore' cookie.")
         return;
     }
     else {
-        console.log("Display cookie message.")
+        // console.log("Display cookie message.")
         displayCookieMessage();       
     }
 }
